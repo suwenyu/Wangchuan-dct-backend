402190cc888cc59b77fe2846ea81bcbd98e99099
suwenyu <cy_i90118@yahoo.com.tw>
2016-02-21 17:25:22 +0800

    database

---
+++ trydjango19/posts/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import(
+	post_create,
+	post_detail,
+	post_list,
+	post_update,
+	post_delete,
+	)
+
+
+urlpatterns = [
+	url(r'^$' , post_list, name='list' ),
+    url(r'^create/$' , post_create ),
+    url(r'^(?P<id>\d+)/$' , post_detail , name='detail' ),
+    url(r'^(?P<id>\d+)/edit/$' , post_update , name="update"),
+    url(r'^(?P<id>\d+)/delete/$' , post_delete ),
+
+]
---
+++ trydjango19/posts/views.py
@@ -0,0 +1,134 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+from urllib import quote_plus
+
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.db.models import Q
+from django.http import HttpResponse ,HttpResponseRedirect ,Http404
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.utils import timezone
+
+from .forms import PostForm
+from .models import Post
+# Create your views here.
+
+
+def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	if not request.user.is_authenticated():
+		raise Http404
+	# if request.methos == "POST":
+	# 	print request.POST
+	form = PostForm(request.POST or None , request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.user = request.user
+		instance.save()
+		messages.success(request, "Successfully Created.")
+		context = {
+			"haha" : instance.get_absolute_url,
+		}
+		return render(request, "qrcode.html" , context)
+	else:
+		messages.error(request, "Not Successfully Created.")
+
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html" , context)
+
+def post_detail(request , id=None): #retrieve
+	# instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post , id=id)
+	if  instance.publish > timezone.now().date() or instance.draft:
+		if not request.user.is_staff or not request.user.is_superuser:
+			raise Http404
+	share_string = instance.content
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"share_string": share_string, 
+	}
+	return render(request, "post_detail.html" , context)
+
+def post_list(request): #list item
+	today = timezone.now().date()
+	queryset_list = Post.objects.active()
+	if request.user.is_staff or request.user.is_superuser:
+		queryset_list = Post.objects.all()
+	query = request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(
+			Q(title__icontains=query) |
+			Q(content__icontains=query) |
+			Q(user__first_name__icontains=query) |
+			Q(user__last_name__icontains=query)|
+			Q(classify__icontains=query)
+			).distinct()
+
+	paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+	page_request_var = "page"
+
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+	    # If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+	    # If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+	context = {
+		"object_list" :  queryset,
+		"title": "List",
+		"page_request_var": page_request_var,
+		"today" : today,
+	}
+	# if request.user.is_authenticated():
+	# 	context = {
+	# 		"title": "My User List"
+	# 	}
+	# else:
+	# 	context = {
+	# 		"title": "List"
+	# 	}
+	return render(request, "post_list.html" , context)
+
+
+
+
+
+
+
+def post_update(request , id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	instance =get_object_or_404(Post , id=id)
+
+	form = PostForm(request.POST or None , request.FILES or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		#message success
+		messages.success(request, "<a href='#'>Item</a> Saved" , extra_tags="html_safe")
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}
+	return render(request, "post_form.html" , context)
+
+def post_delete(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	instance =get_object_or_404(Post , id=id)
+	instance.delete()
+	messages.success(request, "Successfully Deleted")
+	return redirect("posts:list")
+
